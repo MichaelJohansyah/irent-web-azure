93f9c335716dc915dde9031558a460e4eebdeddd
Michael <johansyahmichael@gmail.com>
2025-06-12 09:37:12 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
+        ]);
+        
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
